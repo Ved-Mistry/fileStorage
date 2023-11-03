b632b433e307f2fb07aa8ff9392d0eadf18cdac5
Ved Mistry <ved@veds-mbp.lan>
2023-11-03 04:45:16 -0700

    Done with first two segments

--- client/client.go
+++ client/client.go
@@ -15,6 +15,7 @@ package client
 // - strings
 
 import (
+	"bytes"
 	"encoding/json"
 
 	userlib "github.com/cs161-staff/project2-userlib"
@@ -23,7 +24,6 @@ import (
 	// hex.EncodeToString(...) is useful for converting []byte to string
 
 	// Useful for string manipulation
-	"strings"
 
 	// Useful for formatting strings (e.g. `fmt.Sprintf`).
 	"fmt"
@@ -109,7 +109,16 @@ func someUsefulThings() {
 // A Go struct is like a Python or Java class - it can have attributes
 // (e.g. like the Username attribute) and methods (e.g. like the StoreFile method below).
 type User struct {
-	Username string
+	UUiD       uuid.UUID
+	Username   string
+	Salt       []byte
+	PassA      []byte
+	PassB      []byte
+	EncPrivKey []byte
+	EncSignKey []byte
+	PubKey     userlib.PublicKeyType
+	privateKey userlib.PrivateKeyType
+	signKey    userlib.PrivateKeyType
 
 	// You can add other attributes here if you want! But note that in order for attributes to
 	// be included when this struct is serialized to/from JSON, they must be capitalized.
@@ -119,48 +128,593 @@ type User struct {
 	// begins with a lowercase letter).
 }
 
+type File struct {
+	Content   []byte
+	Creator   userlib.UUID
+	PrevBlock userlib.UUID
+}
+
+type SignedFile struct {
+	FileData  []byte
+	Signature []byte
+}
+
+type FilePointer struct { //Gets encrypted with Editor's public key
+	F_owner uuid.UUID
+	RandKey []byte
+}
+
+type BlockPointer struct { //Gets encrypted with RandKey
+	BlockiD uuid.UUID
+	SymKey  []byte
+}
+
+type AccessList struct {
+	Fname    uuid.UUID   //Local name for this file
+	Children []uuid.UUID //Everyone they have directly shared with
+}
+
 // NOTE: The following methods have toy (insecure!) implementations.
 
+type Err int
+
+func (e Err) Error() string {
+	if e == 1 {
+		return "Username cannot be empty"
+	}
+	if e == 2 {
+		return "Username is taken"
+	}
+	if e == 3 {
+		return "Error with built-in function"
+	}
+	if e == 4 {
+		return "Username does not exist"
+	}
+	if e == 5 {
+		return "Incorrect credentials"
+	}
+	if e == 6 {
+		return "Tampering Detected"
+	}
+	if e == 7 {
+		return "SymEnc Sizing Issues"
+	}
+	if e == 8 {
+		return "File Not Found"
+	}
+	return ""
+}
+
+// Used Chat GPT to create this helper function
+func reverseBytes(input []byte) []byte {
+	for i, j := 0, len(input)-1; i < j; i, j = i+1, j-1 {
+		input[i], input[j] = input[j], input[i]
+	}
+	return input
+}
+
 func InitUser(username string, password string) (userdataptr *User, err error) {
+
 	var userdata User
+	if username == "" {
+		return &userdata, Err(1)
+	}
+
+	user_n, E := uuid.FromBytes(userlib.Hash([]byte(username))[:16])
+	if E != nil {
+		return &userdata, Err(3)
+	}
+
+	_, bol := userlib.DatastoreGet(user_n)
+	if bol {
+		return &userdata, Err(2)
+	}
+
+	U := uuid.New()
+	salt := userlib.RandomBytes(16)
+
+	pubKey, prKey, E := userlib.PKEKeyGen()
+	if E != nil {
+		return &userdata, Err(3)
+	}
+
+	sKey, vKey, E := userlib.DSKeyGen()
+	if E != nil {
+		return &userdata, Err(3)
+	}
+
+	userlib.KeystoreSet("pk"+U.String(), pubKey)
+	userlib.KeystoreSet("vk"+U.String(), vKey)
+
+	HPass := userlib.Argon2Key([]byte(password), salt, 16)
+
+	pSave, E := json.Marshal(prKey)
+	if E != nil {
+		return &userdata, Err(3)
+	}
+
+	sSave, E := json.Marshal(sKey)
+	if E != nil {
+		return &userdata, Err(3)
+	}
+
+	if len(HPass) != 16 || len([]byte(U.String())) < 16 {
+		fmt.Println("c2")
+		return &userdata, Err(7)
+	}
+	priv := userlib.SymEnc(HPass, []byte(U.String())[:16], pSave)
+	sign := userlib.SymEnc(HPass, []byte(U.String())[:16], sSave)
+
+	A, E := userlib.PKEEnc(pubKey, HPass)
+	if E != nil {
+		return &userdata, Err(3)
+	}
+
+	B, E := userlib.PKEEnc(pubKey, userlib.Argon2Key(HPass, salt, 16))
+	if E != nil {
+		return &userdata, Err(3)
+	}
+
+	userdata.UUiD = U
 	userdata.Username = username
+	userdata.Salt = salt
+	userdata.PassA = A
+	userdata.PassB = B
+	userdata.PubKey = pubKey
+	userdata.EncPrivKey = priv
+	userdata.EncSignKey = sign
+
+	toSave, E := json.Marshal(userdata)
+	if E != nil {
+		return &userdata, Err(3)
+	}
+
+	userlib.DatastoreSet(user_n, toSave)
+
+	userdata.privateKey = prKey
+	userdata.signKey = sKey
+
 	return &userdata, nil
 }
 
 func GetUser(username string, password string) (userdataptr *User, err error) {
 	var userdata User
-	userdataptr = &userdata
-	return userdataptr, nil
+
+	user_n, E := uuid.FromBytes(userlib.Hash([]byte(username))[:16])
+	if E != nil {
+		return &userdata, Err(3)
+	}
+
+	data, found := userlib.DatastoreGet(user_n)
+	if !found {
+		fmt.Println("E1")
+		return &userdata, Err(2)
+	}
+
+	E = json.Unmarshal(data, &userdata)
+	if E != nil {
+		fmt.Println("E2")
+		return nil, Err(6)
+	}
+
+	salt := userdata.Salt
+	PassA := userdata.PassA
+	PassB := userdata.PassB
+	priv := userdata.EncPrivKey
+	sign := userdata.EncSignKey
+
+	HPass := userlib.Argon2Key([]byte(password), salt, 16)
+
+	if len(HPass) != 16 || len(priv) < 16 || len(sign) < 16 {
+		fmt.Println("E4")
+		return nil, Err(7)
+	}
+
+	pSave := userlib.SymDec(HPass, priv)
+	sSave := userlib.SymDec(HPass, sign)
+
+	var prKey userlib.PrivateKeyType
+	var sKey userlib.PrivateKeyType
+
+	E = json.Unmarshal(pSave, &prKey)
+	if E != nil {
+		return nil, Err(3)
+	}
+	E = json.Unmarshal(sSave, &sKey)
+	if E != nil {
+		return nil, Err(3)
+	}
+
+	HPTest, E := userlib.PKEDec(prKey, PassA)
+	if E != nil {
+		return nil, Err(3)
+	}
+	BTest := userlib.Argon2Key(HPTest, salt, 16)
+	decB, E := userlib.PKEDec(prKey, PassB)
+	if E != nil {
+		return nil, Err(3)
+	}
+
+	if !bytes.Equal(BTest, decB) {
+		fmt.Println("E5")
+		return nil, Err(6)
+	}
+
+	if !bytes.Equal(HPass, HPTest) {
+		fmt.Println("E6")
+		return nil, Err(5)
+	}
+
+	userdata.privateKey = prKey
+	userdata.signKey = sKey
+
+	return &userdata, nil
 }
 
 func (userdata *User) StoreFile(filename string, content []byte) (err error) {
-	storageKey, err := uuid.FromBytes(userlib.Hash([]byte(filename + userdata.Username))[:16])
-	if err != nil {
-		return err
+	f_editor, E := uuid.FromBytes(userlib.Hash([]byte(filename + userdata.UUiD.String()))[:16])
+	if E != nil {
+		return Err(3)
 	}
-	contentBytes, err := json.Marshal(content)
-	if err != nil {
-		return err
+
+	var f_owner uuid.UUID
+	var RandKey []byte
+	var symKey []byte
+
+	info, bol := userlib.DatastoreGet(f_editor)
+
+	found := false
+
+	if bol {
+		found = true
+		fpdata, E := userlib.PKEDec(userdata.privateKey, info)
+		if E != nil {
+			return Err(3)
+		}
+		var filepointer FilePointer
+		E = json.Unmarshal(fpdata, &filepointer)
+		if E != nil {
+			return Err(3)
+		}
+
+		f_owner = filepointer.F_owner
+		RandKey = filepointer.RandKey
+
+		info, bol := userlib.DatastoreGet(f_owner)
+		if !bol {
+			return Err(3)
+		}
+
+		if len(RandKey) != 16 || len(info) < 16 {
+			fmt.Println("case 1")
+			return Err(7)
+		}
+		bpdata := userlib.SymDec(RandKey, info)
+		var blockpointer BlockPointer
+		E = json.Unmarshal(bpdata, &blockpointer)
+		if E != nil {
+			return Err(3)
+		}
+
+		symKey = blockpointer.SymKey
+		return nil
+	} else {
+		RandKey, E = userlib.HashKDF(userdata.EncPrivKey[:16], userlib.RandomBytes(16))
+		if E != nil {
+			return Err(3)
+		}
+		RandKey = RandKey[:16]
+
+		f_owner, E = uuid.FromBytes(userlib.Hash(append([]byte(f_editor.String()), []byte(userdata.UUiD.String())...))[:16])
+		if E != nil {
+			return Err(3)
+		}
+
+		symKey, E = userlib.HashKDF(userdata.EncPrivKey[:16], userlib.RandomBytes(16))
+		if E != nil {
+			return Err(3)
+		}
+		symKey = symKey[:16]
 	}
-	userlib.DatastoreSet(storageKey, contentBytes)
-	return
+
+	BlockiD := uuid.New()
+
+	if len(symKey) < 16 || len([]byte(userdata.UUiD.String())) < 16 {
+		fmt.Println("case 2")
+		return Err(7)
+	}
+	cont := userlib.SymEnc(symKey, []byte(userdata.UUiD.String())[:16], content)
+
+	var file File
+	file.Content = cont
+	file.Creator = userdata.UUiD
+	file.PrevBlock = f_owner
+
+	fBlock, E := json.Marshal(file)
+	if E != nil {
+		return Err(3)
+	}
+	signature, E := userlib.DSSign(userdata.signKey, fBlock)
+	if E != nil {
+		return Err(3)
+	}
+	var sigFile SignedFile
+	sigFile.FileData, sigFile.Signature = fBlock, signature
+	toSave, E := json.Marshal(sigFile)
+	if E != nil {
+		return Err(3)
+	}
+	if len([]byte(f_owner.String())) < 16 || len([]byte(userdata.UUiD.String())) < 16 {
+		fmt.Println("case 5")
+		return Err(7)
+	}
+	contentBytes := userlib.SymEnc([]byte(f_owner.String())[:16], []byte(userdata.UUiD.String())[:16], toSave)
+	userlib.DatastoreSet(BlockiD, contentBytes)
+
+	var bpointer BlockPointer
+	bpointer.BlockiD = BlockiD
+	bpointer.SymKey = symKey
+	toSave, E = json.Marshal(bpointer)
+	if E != nil {
+		return Err(3)
+	}
+	if len(RandKey) != 16 || len([]byte(userdata.UUiD.String())) < 16 {
+		fmt.Println("case 3")
+		return Err(7)
+	}
+	contentBytes = userlib.SymEnc(RandKey, []byte(userdata.UUiD.String())[:16], toSave)
+	userlib.DatastoreSet(f_owner, contentBytes)
+
+	if found {
+		return nil
+	}
+
+	var fpointer FilePointer
+	fpointer.F_owner = f_owner
+	fpointer.RandKey = RandKey
+	//------------------
+	toSave, E = json.Marshal(fpointer)
+	if E != nil {
+		return Err(3)
+	}
+	contentBytes, E = userlib.PKEEnc(userdata.PubKey, toSave)
+	if E != nil {
+		return Err(3)
+	}
+	userlib.DatastoreSet(f_editor, contentBytes)
+
+	var accessList AccessList
+	accessList.Fname = f_owner
+	toSave, E = json.Marshal(accessList)
+	if E != nil {
+		return Err(3)
+	}
+	if len(RandKey) != 16 || len([]byte(userdata.UUiD.String())) < 16 {
+		fmt.Println("case 4")
+		return Err(7)
+	}
+	contentBytes = userlib.SymEnc(RandKey, []byte(userdata.UUiD.String())[:16], toSave)
+	Lbytes, E := userlib.HashKDF(userdata.EncPrivKey[:16], []byte("AccessList"))
+	if E != nil {
+		return Err(3)
+	}
+	privateUUID, E := uuid.FromBytes(Lbytes[:16])
+	if E != nil {
+		return Err(3)
+	}
+	userlib.DatastoreSet(privateUUID, contentBytes)
+
+	return nil
 }
 
 func (userdata *User) AppendToFile(filename string, content []byte) error {
+	f_editor, E := uuid.FromBytes(userlib.Hash([]byte(filename + userdata.UUiD.String()))[:16])
+	if E != nil {
+		fmt.Println("A1")
+		return Err(3)
+	}
+
+	info, bol := userlib.DatastoreGet(f_editor)
+	if !bol {
+		fmt.Println("A2")
+		return Err(8)
+	}
+
+	var f_owner uuid.UUID
+	var RandKey []byte
+	var oldBlockiD uuid.UUID
+	var symKey []byte
+	BlockiD := uuid.New()
+
+	fpdata, E := userlib.PKEDec(userdata.privateKey, info)
+	if E != nil {
+		fmt.Println("A3")
+		return Err(3)
+	}
+	var filepointer FilePointer
+	E = json.Unmarshal(fpdata, &filepointer)
+	if E != nil {
+		fmt.Println("A4")
+		return Err(3)
+	}
+
+	f_owner = filepointer.F_owner
+	RandKey = filepointer.RandKey
+
+	//-------
+
+	info, bol = userlib.DatastoreGet(f_owner)
+	if !bol {
+		fmt.Println("A5")
+		return Err(3)
+	}
+
+	if len(RandKey) != 16 || len(info) < 16 {
+		fmt.Println("A6")
+		return Err(7)
+	}
+	bpdata := userlib.SymDec(RandKey, info)
+	var blockpointer BlockPointer
+	E = json.Unmarshal(bpdata, &blockpointer)
+	if E != nil {
+		fmt.Println("A7 ", E)
+		return Err(3)
+	}
+
+	symKey = blockpointer.SymKey
+	oldBlockiD = blockpointer.BlockiD
+
+	if len(symKey) != 16 || len([]byte(userdata.UUiD.String())) < 16 {
+		fmt.Println("A8")
+		return Err(7)
+	}
+	cont := userlib.SymEnc(symKey, []byte(userdata.UUiD.String())[:16], content)
+
+	var file File
+	file.Content = cont
+	file.Creator = userdata.UUiD
+	file.PrevBlock = oldBlockiD
+
+	fBlock, E := json.Marshal(file)
+	if E != nil {
+		fmt.Println("A14")
+		return Err(3)
+	}
+	signature, E := userlib.DSSign(userdata.signKey, fBlock)
+	if E != nil {
+		fmt.Println("A9")
+		return Err(3)
+	}
+	var sigFile SignedFile
+	sigFile.FileData, sigFile.Signature = fBlock, signature
+	toSave, E := json.Marshal(sigFile)
+	if E != nil {
+		fmt.Println("A10")
+		return Err(3)
+	}
+	if len([]byte(f_owner.String())) < 16 || len([]byte(userdata.UUiD.String())) < 16 {
+		fmt.Println("A11")
+		return Err(7)
+	}
+	contentBytes := userlib.SymEnc([]byte(f_owner.String())[:16], []byte(userdata.UUiD.String())[:16], toSave)
+	userlib.DatastoreSet(BlockiD, contentBytes)
+
+	blockpointer.BlockiD = BlockiD
+	toSave, E = json.Marshal(blockpointer)
+	if E != nil {
+		fmt.Println("A12")
+		return Err(3)
+	}
+	if len(RandKey) != 16 || len([]byte(userdata.UUiD.String())) < 16 {
+		fmt.Println("c3")
+		fmt.Println("A13")
+		return Err(7)
+	}
+	contentBytes = userlib.SymEnc(RandKey, []byte(userdata.UUiD.String())[:16], toSave)
+	userlib.DatastoreSet(f_owner, contentBytes)
+
 	return nil
 }
 
 func (userdata *User) LoadFile(filename string) (content []byte, err error) {
-	storageKey, err := uuid.FromBytes(userlib.Hash([]byte(filename + userdata.Username))[:16])
-	if err != nil {
-		return nil, err
+
+	var to_return []byte
+
+	f_editor, E := uuid.FromBytes(userlib.Hash([]byte(filename + userdata.UUiD.String()))[:16])
+	if E != nil {
+		return to_return, Err(3)
 	}
-	dataJSON, ok := userlib.DatastoreGet(storageKey)
-	if !ok {
-		return nil, errors.New(strings.ToTitle("file not found"))
+
+	info, bol := userlib.DatastoreGet(f_editor)
+	if !bol {
+		return to_return, Err(8)
 	}
-	err = json.Unmarshal(dataJSON, &content)
-	return content, err
+
+	var f_owner uuid.UUID
+	var RandKey []byte
+	var BlockiD uuid.UUID
+	var symKey []byte
+
+	fpdata, E := userlib.PKEDec(userdata.privateKey, info)
+	if E != nil {
+		return to_return, Err(3)
+	}
+	var filepointer FilePointer
+	E = json.Unmarshal(fpdata, &filepointer)
+	if E != nil {
+		return to_return, Err(3)
+	}
+
+	f_owner = filepointer.F_owner
+	RandKey = filepointer.RandKey
+
+	info, bol = userlib.DatastoreGet(f_owner)
+	if !bol {
+		return to_return, Err(3)
+	}
+
+	if len(RandKey) != 16 || len(info) < 16 {
+		fmt.Println("L1")
+		return to_return, Err(7)
+	}
+	bpdata := userlib.SymDec(RandKey, info)
+	var blockpointer BlockPointer
+	E = json.Unmarshal(bpdata, &blockpointer)
+	if E != nil {
+		return to_return, Err(3)
+	}
+
+	symKey = blockpointer.SymKey
+	BlockiD = blockpointer.BlockiD
+
+	var cSum []byte
+
+	for BlockiD != f_owner {
+		info, bol = userlib.DatastoreGet(BlockiD)
+		if !bol {
+			return to_return, Err(3)
+		}
+		if len([]byte(f_owner.String())) < 16 || len(info) < 16 {
+			fmt.Println("L2")
+			return to_return, Err(7)
+		}
+		Tupl := userlib.SymDec([]byte(f_owner.String())[:16], info)
+		var sigFile SignedFile
+		E = json.Unmarshal(Tupl, &sigFile)
+		if E != nil {
+			return to_return, Err(3)
+		}
+		filedata := sigFile.FileData
+		var file File
+		E = json.Unmarshal(filedata, &file)
+		if E != nil {
+			return to_return, Err(6)
+		}
+		signature := sigFile.Signature
+		vKey, bol := userlib.KeystoreGet("vk" + file.Creator.String())
+		if !bol {
+			return to_return, Err(3)
+		}
+		E = userlib.DSVerify(vKey, filedata, signature)
+		if E != nil {
+			return to_return, Err(6)
+		}
+
+		if len(symKey) != 16 || len(file.Content) < 16 {
+			fmt.Println("L3")
+			return to_return, Err(7)
+		}
+		toAdd := userlib.SymDec(symKey, file.Content)
+		toAdd = reverseBytes(toAdd)
+		cSum = append(cSum, toAdd...)
+
+		BlockiD = file.PrevBlock
+	}
+
+	to_return = reverseBytes(cSum)
+
+	return to_return, nil
 }
 
 func (userdata *User) CreateInvitation(filename string, recipientUsername string) (
